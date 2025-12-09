15c7251dfa76f5aced992f3f375e007f44208af0
NegusIII <sergiosalvadorgil.ccajagranada@gmail.com>
2025-12-09 12:31:15 +0100

    inicio letras

--- practica-final-cifrasyletras_prado/practica-final-letras/include/dictionary.h
+++ practica-final-cifrasyletras_prado/practica-final-letras/include/dictionary.h
@@ -1,10 +1,89 @@
-#ifndef __DICTIONARY_H__
-#define __DICTIONARY_H__
+#ifndef __Diccionario_h__
+#define __Diccionario_h__
+#include <set>
+#include <string>
+#include <vector>
 
-/**
- * @brief TDA Dictionary
- * @details Almacena las palabras de un fichero de texto y permite iterar sobre ellas
- *
- */
+using namespace std;
 
+class Diccionario{
+private:
+    set<string> datos;
+public:
+    /**
+    @brief Construye un diccionario vacío.
+    **/
+    Diccionario()
+    /**
+    8
+    #Letra Cantidad Puntos
+    A 12 1
+    E 12 1
+    O 9 1
+    I 6 1
+    S 6 1
+    N 5 1
+    L 1 1
+    R 6 1
+    U 5 1
+    T 4 1
+    D 5 2
+    G 2 2
+    C 5 3
+    B 2 3
+    M 2 3
+    P 2 3
+    H 2 4
+    F 1 4
+    V 1 4
+    Y 1 4
+    Q 1 5
+    J 1 8
+    X 1 8
+    Z 1 10
+    @brief Devuelve el numero de palabras en el diccionario
+    **/
+    int size() const ;
+    /**
+    @brief Obtiene todas las palabras en el diccionario de un longitud dada
+    @param longitud: la longitud de las palabras de salida
+    @return un vector con las palabras de longitud especifica en el parametro de entrada
+    **/
+    vector<string> PalabrasLongitud(int longitud);
+    /**
+    @brief Indica si una palabra está en el diccionario o no
+    @param palabra: la palabra que se quiere buscar
+    @return true si la palabra esta en el diccionario. False en caso contrario
+    **/
+    bool Esta(string palabra);
+    /**
+    @brief Lee de un flujo de entrada un diccionario
+    @param is:flujo de entrada
+    @param D: el objeto donde se realiza la lectura.
+    @return el flujo de entrada
+    **/
+    friend istream & operator>>(istream & is,Diccionario &D);
+    /**
+    @brief Escribe en un flujo de salida un diccionario
+    @param os:flujo de salida
+    @param D: el objeto diccionario que se escribe
+    @return el flujo de salida
+    **/
+    friend ostream & operator<<(ostream & os, const Diccionario &D);
+
+    class iterator{
+        private:
+            set<string>::iterator it;
+        public:
+            iterator ();
+
+            string operator *();
+            iterator & operator ++();
+            bool operator ==(const iterator &i)
+            bool operator !=(const iterator &i)
+            friend class Diccionario;
+        };
+        iterator begin();
+        iterator end();
+};
 #endif
--- practica-final-cifrasyletras_prado/practica-final-letras/src/dictionary.cpp
+++ practica-final-cifrasyletras_prado/practica-final-letras/src/dictionary.cpp
@@ -1,1 +1,17 @@
 #include "dictionary.h"
+
+Diccionario::Diccionario():datos(){}
+int Diccionario::size() const {
+
+    return datos.size();
+}
+vector<string> Diccionario::PalabrasLongitud(int longitud) {
+
+    vector<string> v;
+
+    for (set<string>::iterator it=datos.begin(); it !=datos.end(); ++it) {
+
+        if (*it->length()==longitud) v.push_back(*it);
+    }
+    return v;
+}
