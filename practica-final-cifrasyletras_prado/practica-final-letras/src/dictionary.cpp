c343a790d6472f3c8c7801940e10a4c8d4d3797b
nestorgc928 <n.gilcasado6@gmail.com>
2025-12-11 11:45:32 +0100

    diccionario.cpp

--- practica-final-cifrasyletras_prado/practica-final-letras/src/dictionary.cpp
+++ practica-final-cifrasyletras_prado/practica-final-letras/src/dictionary.cpp
@@ -1,8 +1,11 @@
 #include "dictionary.h"
 
-Diccionario::Diccionario():datos(){}
-int Diccionario::size() const {
+#include <set>
+#include <string>
 
+Diccionario::Diccionario():datos(){}
+
+int Diccionario::size() const {
     return datos.size();
 }
 vector<string> Diccionario::PalabrasLongitud(int longitud) {
@@ -14,4 +17,57 @@ vector<string> Diccionario::PalabrasLongitud(int longitud) {
         if (*it->length()==longitud) v.push_back(*it);
     }
     return v;
+}
+
+bool Diccionario::Esta(string palabra) {
+    return (datos.find() != datos.end());
+}
+
+//METODOS DE LA CLASE ITERATOR
+Diccionario::iterator::operator*{
+    return *it;
+}
+
+Diccionario::iterator & Diccionario::iterator::operator++() {
+    ++it;
+    return *this;
+}
+
+bool Diccionario::iterator::operator==(const iterator &i) {
+    return it == i.it;
+}
+
+bool Diccionario::iterator::operator!=(const iterator &i) {
+    return it != i.it;
+}
+
+//GENERADORES DE ITERADORES
+Diccionario::iterator Diccionario::begin() {
+    Diccionario::iterator i;
+    i.it = datos.begin();
+    return i;
+}
+
+Diccionario::iterator Diccionario::end() {
+    Diccionario::iterator i;
+    i.it = datos.end();
+    return i;
+}
+
+//OPERADORES DE ENTRADA Y SALIDA
+istream & operator>>(istream & is, Diccionario &D) {
+    string palabra;
+    while (is>>palabra) {
+        D.datos.insert(palabra);
+    }
+    return is;
+}
+
+ostream & operator<<(ostream & os, Diccionario &D) {
+    set<string>::const_iterator it;
+
+    for (it = D.datos.begin(); it != D.datos.end(); ++it) {
+        os << *it << endl;
+    }
+    return os;
 }
