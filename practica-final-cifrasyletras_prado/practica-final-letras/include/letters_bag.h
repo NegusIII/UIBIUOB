c0646d03b0297bc78d7288bd0608d5701a067c87
NegusIII <sergiosalvadorgil.ccajagranada@gmail.com>
2025-12-14 23:47:38 +0100

    letterbag

--- practica-final-cifrasyletras_prado/practica-final-letras/include/letters_bag.h
+++ practica-final-cifrasyletras_prado/practica-final-letras/include/letters_bag.h
@@ -1,11 +1,64 @@
 #ifndef __LETTERS_BAG_H__
 #define __LETTERS_BAG_H__
 
+#include <list>
+#include <vector>
+#include <iostream>
+
+
 /**
  * @brief TDA LettersBag
  *
  * Este TDA almacena un conjunto de chars utilizado en el juego de letras.
  * La estructura de datos subyacente es una lista de chars.
  */
+
+class LettersBag {
+
+    private:
+        std::list<char> letras;
+
+    public:
+        /**
+         * @brief Constructor por defecto
+         */
+        LettersBag();
+
+        /**
+         * @brief metodo que inserta un caracter en la bolsa
+         *
+         * @param c
+         */
+        void insertar(char c);
+
+        /**
+         *
+         * @return un caracter aleatorio de la bolsa
+         */
+        char extraer();
+
+
+        /**
+         *
+         * @return el tamaño de la bolsa
+         */
+        int size(); const
+
+
+        /**
+         * @brief vacía la bolsa
+         */
+        void clear();
+
+        /**
+         *
+         * @param n numero de letras a extraer
+         * @return un vector con n letras extraídas aleatoriamente
+         * @pre n<size()
+         */
+        std::vector<char> extraer_varias(int n);
+
+        friend std::ostream & operator<<(std::ostream & os, const LettersBag & bag);
+};
 
 #endif
--- practica-final-cifrasyletras_prado/practica-final-letras/src/letters_bag.cpp
+++ practica-final-cifrasyletras_prado/practica-final-letras/src/letters_bag.cpp
@@ -1,1 +1,54 @@
 #include "letters_bag.h"
+#include <random>
+#include <vector>
+
+using namespace std;
+
+LettersBag::LettersBag() {}
+
+void LettersBag::insertar(char c) {
+    letras.push_back(c);
+}
+
+char LettersBag::extraer() {
+
+    list<char>::iterator it = letras.begin();
+
+    static random_device rd;
+    static mt19937 gen(rd());
+    uniform_int_distribution<> dis(0, letras.size() - 1);
+    int pasos = dis(gen);
+
+    std::advance(it, pasos);
+    char c = *it;
+    letras.erase(it);
+
+    return c;
+}
+
+int LettersBag::size() const {
+    return letras.size();
+}
+
+const void LettersBag::clear() {
+
+    letras.clear();
+}
+
+vector<char> LettersBag::extraer_varias(int n) {
+
+    vector<char> v;
+    for (int i = 0; i < n; i++) {
+        v.push_back(extraer());
+    }
+    return v;
+}
+
+ostream & operator<<(ostream & os, const LettersBag & bag) {
+
+    list<char>::const_iterator it;
+    for (it = bag.letras.begin(); it != bag.letras.end(); ++it) {
+        os << *it << endl;
+    }
+    return os;
+}
